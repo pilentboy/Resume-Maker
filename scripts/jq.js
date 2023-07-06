246fd7b3cc86e9746cddd413c8fcf53684ae52e2
pilentboy <sirxmahdi79@gmail.com>
2023-07-07 02:57:53 +0430

    V1.6 add new btn for making new social media box

---
+++ scripts/jq.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    //--------------ADDING A NEW SOCIAL MEDIA BOX 
+    $("#addMoreSMBoxBTN").click(function (e) { 
+       
+        let socialMediaBox=$("#soialMediaBox").clone();
+        
+        $(socialMediaBox).removeClass("d-none");
+
+        $("#addMoreBox").before(socialMediaBox);
+        console.log("test")
+        
+    });
+    //--------------
+
+});
+
+
